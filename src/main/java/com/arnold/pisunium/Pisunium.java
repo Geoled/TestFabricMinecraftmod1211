ae4f45389b44cadba8dda1a7d323570ee8e9c8e5
Geoled <iarikmehanik2009@gmail.com>
2025-12-25 03:45:00 +0300

    Initial commit

---
+++ src/main/java/com/arnold/pisunium/Pisunium.java
@@ -0,0 +1,18 @@
+package com.arnold.pisunium;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Pisunium implements ModInitializer {
+	public static final String MOD_ID = "pisunium";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/arnold/pisunium/PisuniumModClient.java
@@ -0,0 +1,10 @@
+package com.arnold.pisunium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PisuniumModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
