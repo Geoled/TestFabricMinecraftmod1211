532e95f3f7618629151f3e0045bd5b2a17e3602b
Geoled <iarikmehanik2009@gmail.com>
2025-12-25 10:27:02 +0300

    n

--- src/main/java/com/arnold/pisunium/Pisunium.java
+++ src/main/java/com/arnold/pisunium/Pisunium.java
@@ -1,5 +1,6 @@
 package com.arnold.pisunium;
 
+import com.arnold.pisunium.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ public class Pisunium implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 	}
 }
